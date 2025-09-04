708808d3cd6a8e7490cf60630b2f0daed2a43818
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-04 11:31:07 +0200

    Fix argument indexing in argument printing logic

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
         else:
             print("{} arguments:".format(argc))
 
-        for i in range(1, argc + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+        for i in range(argc):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
