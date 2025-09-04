8ba5cd4088a6cfa171b42c8fcfd422f277bbd7ef
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-04 11:28:58 +0200

    Refactor argument printing logic for clarity and consistency

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,10 +6,11 @@ if __name__ == "__main__":
 
     if argc == 0:
         print("0 arguments.")
-    elif argc == 1:
-        print("1 argument:")
     else:
-        print("{} arguments:".format(argc))
+        if argc == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(argc))
 
-    for i, arg in enumerate(sys.argv[1:], 1):
-        print("{}: {}".format(i, arg))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
