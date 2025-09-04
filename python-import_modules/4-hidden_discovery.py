872f454d179739891a1e95b77ffa63048daa8066
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-04 12:13:14 +0200

    Move import statement for importlib.util inside the main block for better encapsulation

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import importlib.util
-
 if __name__ == "__main__":
+    import importlib.util
     spec = importlib.util.spec_from_file_location(
         "hidden_4", "/tmp/hidden_4.pyc")
     if spec is not None and spec.loader is not None:
