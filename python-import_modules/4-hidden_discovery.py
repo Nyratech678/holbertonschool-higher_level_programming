99b9810a338a8840fa4749e3c518c8ce4c2598c5
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-04 11:49:09 +0200

    Move import statement for SourcelessFileLoader inside the main block for better encapsulation

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from importlib.machinery import SourcelessFileLoader
-
 if __name__ == "__main__":
+    from importlib.machinery import SourcelessFileLoader
     loader = SourcelessFileLoader("hidden_4", "/tmp/hidden_4.pyc")
     module = loader.load_module()
     names = [name for name in dir(module) if not name.startswith("__")]
