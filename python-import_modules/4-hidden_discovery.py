e98eff139e31191729eff356bed2d3c71becbc5b
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-04 12:01:32 +0200

    Refactor hidden discovery script to use importlib.util for module loading

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-from importlib.machinery import SourcelessFileLoader
+import importlib.util
 
 if __name__ == "__main__":
-    module = SourcelessFileLoader("hidden_4", "/tmp/hidden_4.pyc").load_module()
-    names = [name for name in dir(module) if not name.startswith("__")]
-    for name in sorted(names):
-        print(name)
+    spec = importlib.util.spec_from_file_location(
+        "hidden_4", "/tmp/hidden_4.pyc")
+    if spec is not None and spec.loader is not None:
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        names = [name for name in dir(module) if not name.startswith("__")]
+        for name in sorted(names):
+            print(name)
