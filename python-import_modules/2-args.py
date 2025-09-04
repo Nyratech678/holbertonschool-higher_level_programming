ab14ffae07f99b38527793b820a49e3f07ce72e1
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-04 11:26:19 +0200

    Remove unnecessary blank line and ensure argument printing logic is correctly formatted

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-
 if __name__ == "__main__":
     argc = len(sys.argv) - 1
 
@@ -11,3 +10,7 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(argc))
+
+    if argc >= 1:
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
