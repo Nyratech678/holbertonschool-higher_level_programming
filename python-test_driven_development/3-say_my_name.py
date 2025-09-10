b1f53c1a5d495383f41275963281e023a86e6e67
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-10 14:16:39 +0200

    Refactor say_my_name function docstring for clarity and consistency

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""This module contains a function that prints a full name."""
+"""This module defines a function to print a full name."""
 
 def say_my_name(first_name, last_name=""):
-    """Prints 'My name is <first_name> <last_name>'.
+    """Print My name is <first_name> <last_name>.
 
     Args:
-        first_name (str): The first name to print.
-        last_name (str, optional): The last name to print. Defaults to "".
+        first_name (str): The first name
+        last_name (str, optional): The last name. Defaults to "".
 
     Raises:
-        TypeError: If first_name or last_name are not strings.
+        TypeError: If first_name or last_name is not a string.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
@@ -17,6 +17,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     if last_name:
-        print("My name is {} {}".format(first_name, last_name))
+        print(f"My name is {first_name} {last_name}")
     else:
-        print("My name is {}".format(first_name))
+        print(f"My name is {first_name}")
