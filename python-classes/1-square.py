b74f6f39e166fed92dbff6a37186f683d7980eab
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-15 09:27:16 +0200

    Implement Square class and main script for testing

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Class that defines a square object."""
+
+    def __init__(self, size):
+        """Initialize the square with a given size."""
+        self.__size = size
