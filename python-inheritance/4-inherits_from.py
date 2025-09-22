c3002c0a84388ecbfffc6d1ccf929505114a2bf0
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-22 10:10:47 +0200

    Add inherits_from function and main script for testing inheritance

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a function that returns True if the object is an instance"""
+
+
+def ihnerits_from(obj, a_class):
+    """Returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False.
+    Args:
+        obj: The object to check.
+        a_class: The class to match the type of obj to.
+    Returns:
+        True if obj is an instance of a class that inherited
+        from a_class; otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
