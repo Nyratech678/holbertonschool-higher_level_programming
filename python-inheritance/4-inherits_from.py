0aceac56794a5c4fbfc99ff1b2034a68f254507c
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-22 10:12:11 +0200

    Refactor inherits_from function for clarity and efficiency

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a function that returns True if the object is an instance"""
+"""Define a function that returns True or False if the object is an instance"""
 
 
-def ihnerits_from(obj, a_class):
+def inherits_from(obj, a_class):
     """Returns True if the object is an instance of a class that
     inherited (directly or indirectly) from the specified class;
     otherwise False.
