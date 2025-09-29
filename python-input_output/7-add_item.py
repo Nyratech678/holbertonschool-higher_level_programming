3a3ecb1d74345f08f19d862d652a2ffbfee0e255
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-29 11:51:54 +0200

    Fix docstring for clarity and improve error handling in 7-add_item module

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that adds all arguments to a Python list,"""
+"""Module that adds all arguments to a Python list, and then saves them to a file"""
 import sys
 from os.path import exists
 
@@ -7,11 +7,14 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+
 if exists(filename):
-    items = load_from_json_file(filename)
+    try:
+        items = load_from_json_file(filename)
+    except Exception:
+        items = []
 else:
     items = []
 
 items.extend(sys.argv[1:])
-
 save_to_json_file(items, filename)
