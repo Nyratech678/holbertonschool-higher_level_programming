ab901240cbecdb6ee84b2e578399850d7358a27b
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-29 13:04:56 +0200

    Add class_to_json module and MyClass definition for JSON serialization

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that returns the dictionary description with simple data structure"""
+import json
+
+
+def class_to_json(obj):
+    """Return the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization
+    of an object"""
+    return obj.__dict__
