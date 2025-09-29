c39df9414a073c2468e5f609a63cd57bbca66a0e
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-29 13:14:19 +0200

    Add Student class with JSON serialization method

---
+++ python-input_output/10-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student"""
+
+
+class Student:
+    """Student class definition"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance
+
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance
+
+        If attrs is a list of strings, only attribute names contained in this
+        list must be retrieved.
+
+        Args:
+            attrs (list, optional): List of attribute names to retrieve.
+                                    Defaults to None.
+
+        Returns:
+            dict: Dictionary representation of the Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            filtered_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    filtered_dict[attr] = getattr(self, attr)
+            return filtered_dict
