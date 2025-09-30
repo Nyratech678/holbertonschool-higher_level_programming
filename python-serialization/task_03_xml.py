f767533386add5bc3f39bd925be96792f849208d
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-09-30 11:59:54 +0200

    Add XML serialization and deserialization functions

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Serializing and Deserializing with XML"""
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """Serialize a dictionary to an XML file."""
+    root = ET.Element("data")  # racine <data>
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)  # clé devient tag
+        child.text = str(value)  # valeur en texte
+
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding="utf-8", xml_declaration=True)
+
+
+def deserialize_from_xml(filename):
+    """Deserialize an XML file to a dictionary."""
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    dictionary = {}
+    for child in root:
+        dictionary[child.tag] = child.text
+
+    return dictionary
