1285b7b93c355f45dc098f958254cf4280904eda
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-10-06 14:41:21 +0200

    Refactor user management logic to simplify user retrieval and improve error handling

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -4,10 +4,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users = {
-    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
-    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
-}
+users = {}
 
 
 @app.route("/")
@@ -30,27 +27,19 @@ def user_profile(username):
     user = users.get(username)
     if user:
         return jsonify(user)
-    else:
-        return jsonify({"error": "User not found"}), 404
+    return jsonify({"error": "User not found"}), 404
 
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
     data = request.get_json()
-    if not data:
-        return jsonify({"error": "Invalid JSON"}), 400
-    if "username" not in data:
+    if not data or "username" not in data:
         return jsonify({"error": "Username is required"}), 400
-
     username = data["username"]
     if username in users:
         return jsonify({"error": "Username already exists"}), 409
-
     users[username] = data
-    return jsonify({
-        "message": "User added",
-        "user": data
-    }), 201
+    return jsonify({"message": "User added", "user": data}), 201
 
 
 if __name__ == "__main__":
