2b63fb993bc19e48dd6d04dbe5a50491eac6dd52
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-10-24 11:57:57 +0200

    Refactor database connection and query execution in select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
 """Lists all states from the database hbtn_0e_0_usa"""
-import MySQLdb
 import sys
+import MySQLdb
 
 
 if __name__ == "__main__":
-    # Retrieve arguments
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Connect to database
-    db = MySQLdb.connect(
-        host="127.0.0.1",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    # Create cursor and execute query
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Print results
-    for row in cursor.fetchall():
+    db = MySQLdb.connect(host="127.0.0.1",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY id ASC")
+    result = c.fetchall()
+    for row in result:
         print(row)
-
-    # Cleanup
-    cursor.close()
+    c.close()
     db.close()
