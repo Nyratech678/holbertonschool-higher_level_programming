5c8449c3f725914f2e0e79c0d8cb8e76a75b2968
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-10-24 11:59:20 +0200

    Refactor database connection code for improved readability in select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,17 +3,17 @@
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="127.0.0.1",
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         port=3306)
+    db = MySQLdb.connect(
+        host="127.0.0.1",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
     c = db.cursor()
     c.execute("SELECT * FROM states ORDER BY id ASC")
-    result = c.fetchall()
-    for row in result:
+    for row in c.fetchall():
         print(row)
     c.close()
     db.close()
